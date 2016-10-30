5e325f17ce836b2e7b87cc527b472be5a28716b3
Marat Shagiev <m_js@bk.ru>
2016-10-30 15:54:54 +0300

    Init project add description and breef in readme.md add .gitignore code is empty

---
+++ core_boxJointsFinger.jsx
@@ -0,0 +1,12 @@
+/**
+ * ExtendScript Adobe Illustrator CS6+
+ * The generator of the box with the edges connectors of the type "finger"
+ * 
+ * core
+ * */
+
+//@target illustrator
+
+(function genBoxFingerJoints() {
+
+}());
